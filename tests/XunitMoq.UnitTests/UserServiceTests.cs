cc738a474056fc7677f4db899a9124a04b869098
Aleksei Mialkin <anmyalkin@gmail.com>
2022-03-28 22:12:14 +0300

    Add test

--- tests/XunitMoq.UnitTests/UserServiceTests.cs
+++ tests/XunitMoq.UnitTests/UserServiceTests.cs
@@ -11,13 +11,51 @@ namespace XunitMoq.UnitTests;
 public class UserServiceTests
 {
     [Fact]
-    public void Get_WhenUserExists_ReturnsUser()
+    public void Get_WhenUserExists_ReturnsUser_IsAny()
     {
         // Arrange.
-        var userId = 20;
+        var userId = 25;
         var userRepository = new Mock<IUserRepository>();
         userRepository
             .Setup(x => x.Get(It.IsAny<int>()))
+            .Returns(new User(userId, It.IsAny<string>(), It.IsAny<DateTime>()));
+
+        var sut = new UserService(userRepository.Object);
+
+        // Act.
+        var user = sut.Get(userId);
+
+        // Assert.
+        user!.Id.Should().Be(userId);
+    }
+    
+    [Fact]
+    public void Get_WhenUserExists_ReturnsUser_Is()
+    {
+        // Arrange.
+        var userId = 25;
+        var userRepository = new Mock<IUserRepository>();
+        userRepository
+            .Setup(x => x.Get(It.Is<int>(y => y > 20)))
+            .Returns(new User(userId, It.IsAny<string>(), It.IsAny<DateTime>()));
+
+        var sut = new UserService(userRepository.Object);
+
+        // Act.
+        var user = sut.Get(userId);
+
+        // Assert.
+        user!.Id.Should().Be(userId);
+    }
+    
+    [Fact]
+    public void Get_WhenUserExists_ReturnsUser_Variable()
+    {
+        // Arrange.
+        var userId = 25;
+        var userRepository = new Mock<IUserRepository>();
+        userRepository
+            .Setup(x => x.Get(25))
             .Returns(new User(userId, It.IsAny<string>(), It.IsAny<DateTime>()));
 
         var sut = new UserService(userRepository.Object);
